5760aaa692fb16ff341843853988200d6b0f17b5
Elizabeth Harrison <elizabethgrace.dot@gmail.com>
2020-12-14 11:36:00 -0500

    Level 1 Solution

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,43 +3,55 @@ let tableData = data;
 let tbody = d3.select("tbody");
 let table = d3.select("table");
 // Console.log the sightings data from data.js
-console.log(data);
-// Step 1: Loop Through `data` and console.log each weather report object
-data.forEach(item => {
-    console.log(item)
-    let row = tbody.append('tr');
-    let datetime = row.append('td')
-    let city = row.append('td')
-    let state = row.append('td')
-    let country = row.append('td')
-    let shape = row.append('td')
-    let durationMinutes = row.append('td')
-    let comments = row.append('td')
-    datetime.text(item.datetime)
-    city.text(item.city)
-    state.text (item.state)
-    country.text(item.country)
-    shape.text(item.shape)
-    durationMinutes.text(item.durationMinutes)
-    comments.text(item.comments)
-       });
+// console.log(data);
+// Step 1: Loop Through data and console.log each weather report object
+function buildtable(data) {
+  data.forEach((item) => {
+    console.log(item);
+    let row = tbody.append("tr");
+    let datetime = row.append("td");
+    let city = row.append("td");
+    let state = row.append("td");
+    let country = row.append("td");
+    let shape = row.append("td");
+    let durationMinutes = row.append("td");
+    let comments = row.append("td");
+    datetime.text(item.datetime);
+    city.text(item.city);
+    state.text(item.state);
+    country.text(item.country);
+    shape.text(item.shape);
+    durationMinutes.text(item.durationMinutes);
+    comments.text(item.comments);
+  });
+}
+buildtable(data);
 
 
-let button = d3.select("filter-btn");
-//Select the form
-let form = d3.select('#form');
-//Create event handlers
+
+//Select the button
+let button = d3.select("#filter-btn");
+// Select the form
+var form = d3.select("#form");
+// Create event handlers
 button.on("click", runEnter);
 form.on("submit", runEnter);
-//Complete event handler function for the form
-function runEnter(){
-    d3.event.preventDefault();    
-    // Select the input element and get the raw HTML code
-    let ufoInput = d3.select("#datetime");    
-    // Get the value property of the input element
-    let inputValue = ufoInput.property("value");  
-    // Use the form input to filter the data by date  
-    let newData = tableData.filter(item => item.datetime === inputValue)};
-    let ufoDate = newData.map(item => item.datetime);
+// Complete the event handler function for the form
+function runEnter() {
+  d3.event.preventDefault();
+  // Select the input element and get the raw HTML node
+  let ufoinput = d3.select("#datetime");
+  // Get the value property of the input element
+  let inputValue = ufoinput.property("value");
+  //   console.log(inputValue)
+  // Use the form input to filter data by date
+  let newData = tableData.filter((item) => item.datetime === inputValue);
+  console.log(newData);
 
-//console.log(newData)
+  tbody.html("");
+  if (inputValue) {
+    buildtable(newData);
+  } else {
+    buildtable(data);
+  }
+}
